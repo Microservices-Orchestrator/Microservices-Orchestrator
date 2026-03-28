1ef6294fa4e57f7e3e2f772fba4cf43d3b8d4278
Fatih Bilgin <bilginfatih311@gmail.com>
2026-03-28 16:12:07 +0300

    test : jwt auth middleware icin 401 tdd red phase testi yazildi.

---
+++ tests/DispatcherGateway.Tests/JwtAuthMiddlewareTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DispatcherGateway.Tests
+{
+    public class JwtAuthMiddlewareTests
+    {
+        [Fact]
+        public async Task InvokeAsync_NoToken_Returns401()
+        {
+            var context = new DefaultHttpContext();
+            RequestDelegate next = (HttpContext ctx) => Task.CompletedTask;
+
+            var middleware = new JwtAuthMiddleware(next);
+
+            await middleware.InvokeAsync(context);
+
+            Assert.Equal(StatusCodes.Status401Unauthorized, context.Response.StatusCode);
+
+        }
+    }
+}
