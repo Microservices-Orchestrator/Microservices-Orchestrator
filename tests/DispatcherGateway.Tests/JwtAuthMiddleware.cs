5df3359115cfec2b59cd1687863144100adf750e
Fatih Bilgin <bilginfatih311@gmail.com>
2026-03-29 20:02:27 +0300

    test: redis log middleware icin tdd red phase testi yazildi ve hata alindi.

---
+++ tests/DispatcherGateway.Tests/JwtAuthMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace DispatcherGateway.Tests
+{
+    internal class JwtAuthMiddleware
+    {
+        private RequestDelegate next;
+
+        public JwtAuthMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        internal async Task InvokeAsync(DefaultHttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/DispatcherGateway.Tests/RequestLogMiddlewareTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DispatcherGateway.Tests
+{
+    public class RequestLogMiddlewareTests
+    {
+        [Fact]
+
+        public async Task InvokeAsyncLogMethod()
+        {
+            var context = new DefaultHttpContext();
+                context.Request.Path = "/api/tehdit";
+
+            RequestDelegate next = (HttpContext ctx) => Task.CompletedTask;
+
+            var mockLogService = new Mock<ILogService>();
+
+            var middleware = new RequestLogMiddleware(next,mockLogService.Object);
+                await middleware.InvokeAsync(context);
+    
+                handlerMock.Verify(x => x.LogRequest(It.IsAny<HttpContext>()), Times.Once);
+        }
+    }
+}
