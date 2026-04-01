21989c4a421ba034f9b49428f1e83eff9cebb189
Fatih Bilgin <bilginfatih311@gmail.com>
2026-04-01 20:18:48 +0300

    test: rate limit icin red phase testi yazildi.

---
+++ tests/DispatcherGateway.Tests/RateLimitMiddlewareTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DispatcherGateway.Tests
+{
+    public class RateLimitMiddlewareTests
+    {
+        [Fact]
+        public void TestRateLimitMiddleware()
+        {
+            var context = new DefaultHttpContext();
+            RequestDelegate requestDelegate = (HttpContext ctx) =>
+            {
+                return Task.CompletedTask;
+            };
+            RateLimitMiddleware middleware = new RateLimitMiddleware(requestDelegate);
+            for (int i = 0; i < 6; i++)
+            {
+                await middleware.InvokeAsync(context);
+            }
+            Assert.Equal(StatusCodes.Status429TooManyRequests, context.Response.StatusCode);
+        }
+    }
+}
